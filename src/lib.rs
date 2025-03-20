e22a40c3c2779cc58e23ece49e91a6ada9ba42da
Kathryn Long <squeeself@gmail.com>
2025-03-19 20:34:26 -0400

    load/unload & enable/disable

--- src/lib.rs
+++ src/lib.rs
@@ -5,24 +5,20 @@ use std::{
     fmt::Debug,
     hash::Hash,
     iter::FusedIterator,
-    ops::Deref,
 };
 
-pub trait PluginManifest<Id>
-where
-    Id: Deref,
-{
-    fn id(&self) -> &<Id as Deref>::Target;
+pub trait PluginManifest<S> {
+    fn id(&self) -> &S;
 }
 
 #[derive(Debug, Default)]
-pub struct SimplePluginManifest<Id = String> {
-    id: Id,
+pub struct SimplePluginManifest<S = String> {
+    id: S,
     description: String,
 }
 
-impl<Id> SimplePluginManifest<Id> {
-    pub fn new(id: Id, description: String) -> Self {
+impl<S> SimplePluginManifest<S> {
+    pub fn new(id: S, description: String) -> Self {
         Self { id, description }
     }
 
@@ -31,31 +27,41 @@ impl<Id> SimplePluginManifest<Id> {
     }
 }
 
-impl<Id> PluginManifest<Id> for SimplePluginManifest<Id>
+impl<S> PluginManifest<S> for SimplePluginManifest<S>
 where
-    Id: Deref,
+    S: AsRef<str>,
 {
-    fn id(&self) -> &<Id as Deref>::Target {
-        self.id.deref()
+    fn id(&self) -> &S {
+        &self.id
     }
 }
 
-pub trait Plugin<Context>: Any + Send + Sync {
-    fn load(&mut self, _: &mut Context) {}
-    fn unload(&mut self, _: &mut Context) {}
-    fn enable(&mut self, _: &mut Context) {}
-    fn disable(&mut self, _: &mut Context) {}
+pub trait Plugin<C>: Any + Send + Sync {
+    fn load(&mut self, _: &mut C) {}
+    fn unload(&mut self, _: &mut C) {}
+    fn enable(&mut self, _: &mut C) {}
+    fn disable(&mut self, _: &mut C) {}
 }
 
-struct PluginState<Manifest, Context> {
-    manifest: Manifest,
-    plugin: Box<dyn Plugin<Context>>,
+struct PluginState<M, C> {
+    manifest: M,
+    plugin: Box<dyn Plugin<C>>,
     enabled: bool,
 }
 
-impl<Manifest, Context> std::fmt::Debug for PluginState<Manifest, Context>
+impl<M, C> PluginState<M, C> {
+    pub fn new(manifest: M, plugin: Box<dyn Plugin<C>>) -> Self {
+        Self {
+            manifest,
+            plugin,
+            enabled: false,
+        }
+    }
+}
+
+impl<M, C> std::fmt::Debug for PluginState<M, C>
 where
-    Manifest: Debug,
+    M: Debug,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("PluginState")
@@ -66,14 +72,14 @@ where
 }
 
 #[derive(Debug, Default)]
-pub struct PluginRegistry<Manifest, Context, Id = String> {
-    plugins: HashMap<Id, PluginState<Manifest, Context>>,
+pub struct PluginRegistry<M, C, S = String> {
+    plugins: HashMap<S, PluginState<M, C>>,
 }
 
-impl<Manifest, Context, Id> PluginRegistry<Manifest, Context, Id>
+impl<M, C, S> PluginRegistry<M, C, S>
 where
-    Manifest: PluginManifest<Id>,
-    Id: Deref,
+    M: PluginManifest<S>,
+    S: AsRef<str>,
 {
     pub fn new() -> Self {
         Self {
@@ -85,21 +91,21 @@ where
         self.plugins.len()
     }
 
-    pub fn plugin_ids(&self) -> PluginIdIter<Manifest, Context, Id> {
+    pub fn plugin_ids(&self) -> PluginIdIter<M, C, S> {
         PluginIdIter {
             iter: self.plugins.keys(),
         }
     }
 }
 
-impl<Manifest, Context, Id> PluginRegistry<Manifest, Context, Id>
+impl<M, C, S> PluginRegistry<M, C, S>
 where
-    Manifest: PluginManifest<Id>,
-    Id: Deref + Eq + Hash,
+    M: PluginManifest<S>,
+    S: AsRef<str> + Eq + Hash,
 {
-    pub fn get_manifest<Q>(&self, id: &Q) -> Option<&Manifest>
+    pub fn get_manifest<Q>(&self, id: &Q) -> Option<&M>
     where
-        Id: Borrow<Q>,
+        S: Borrow<Q>,
         Q: Eq + Hash,
     {
         self.plugins.get(id).map(|s| &s.manifest)
@@ -107,42 +113,117 @@ where
 
     pub fn is_enabled<Q>(&self, id: &Q) -> Option<bool>
     where
-        Id: Borrow<Q>,
+        S: Borrow<Q>,
         Q: Eq + Hash,
     {
         self.plugins.get(id).map(|s| s.enabled)
     }
 
-    pub fn get_plugin<Q>(&self, id: &Q) -> Option<&dyn Plugin<Context>>
+    pub fn get_plugin<Q>(&self, id: &Q) -> Option<&dyn Plugin<C>>
     where
-        Id: Borrow<Q>,
+        S: Borrow<Q>,
         Q: Eq + Hash,
     {
         self.plugins.get(id).map(|s| s.plugin.as_ref())
     }
 
-    pub fn get_plugin_mut<Q>(&mut self, id: &Q) -> Option<&mut dyn Plugin<Context>>
+    pub fn get_plugin_mut<Q>(&mut self, id: &Q) -> Option<&mut dyn Plugin<C>>
     where
-        Id: Borrow<Q>,
+        S: Borrow<Q>,
         Q: Eq + Hash,
     {
         self.plugins.get_mut(id).map(|s| s.plugin.as_mut())
     }
+
+    pub fn load_plugin<Q>(&mut self, manifest: M, plugin: Q, context: &mut C) -> bool
+    where
+        Q: Into<Box<dyn Plugin<C>>>,
+        S: Clone,
+        C: 'static,
+    {
+        let id = manifest.id().clone();
+        if self.plugins.contains_key(&id) {
+            false
+        } else {
+            // TODO: Clean up these clones
+            let id = manifest.id().clone();
+            self.plugins
+                .insert(id.clone(), PluginState::new(manifest, plugin.into()))
+                .unwrap();
+            let plugin = self.get_plugin_mut(&id).unwrap();
+            plugin.load(context);
+
+            true
+        }
+    }
+
+    pub fn unload_plugin<Q>(&mut self, id: &Q, context: &mut C) -> bool
+    where
+        S: Borrow<Q>,
+        Q: Eq + Hash,
+        C: 'static,
+    {
+        if let Some(state) = self.plugins.get_mut(id) {
+            state.plugin.unload(context);
+            self.plugins.remove(id);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn enable_plugin<Q>(&mut self, id: &Q, context: &mut C) -> bool
+    where
+        S: Borrow<Q>,
+        Q: Eq + Hash,
+        C: 'static,
+    {
+        if let Some(state) = self.plugins.get_mut(id) {
+            if !state.enabled {
+                state.plugin.enable(context);
+                state.enabled = true;
+                true
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    }
+
+    pub fn disable_plugin<Q>(&mut self, id: &Q, context: &mut C) -> bool
+    where
+        S: Borrow<Q>,
+        Q: Eq + Hash,
+        C: 'static,
+    {
+        if let Some(state) = self.plugins.get_mut(id) {
+            if state.enabled {
+                state.enabled = false;
+                state.plugin.disable(context);
+                true
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    }
 }
 
 #[derive(Debug)]
-pub struct PluginIdIter<'a, Manifest, Context, Id> {
-    iter: hash_map::Keys<'a, Id, PluginState<Manifest, Context>>,
+pub struct PluginIdIter<'a, M, C, S> {
+    iter: hash_map::Keys<'a, S, PluginState<M, C>>,
 }
 
-impl<'a, Manifest, Context, Id> Iterator for PluginIdIter<'a, Manifest, Context, Id>
+impl<'a, M, C, S> Iterator for PluginIdIter<'a, M, C, S>
 where
-    Id: Deref,
+    S: AsRef<str>,
 {
-    type Item = &'a <Id as Deref>::Target;
+    type Item = &'a str;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.iter.next().map(|id| id.deref())
+        self.iter.next().map(|id| id.as_ref())
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
@@ -157,12 +238,6 @@ where
     }
 }
 
-impl<'a, Manifest, Context, Id> FusedIterator for PluginIdIter<'a, Manifest, Context, Id> where
-    Id: Deref
-{
-}
+impl<'a, M, C, S> FusedIterator for PluginIdIter<'a, M, C, S> where S: AsRef<str> {}
 
-impl<'a, Manifest, Context, Id> ExactSizeIterator for PluginIdIter<'a, Manifest, Context, Id> where
-    Id: Deref
-{
-}
+impl<'a, M, C, S> ExactSizeIterator for PluginIdIter<'a, M, C, S> where S: AsRef<str> {}
